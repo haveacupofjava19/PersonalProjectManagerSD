76be8f82d7e27ebb63117f52005e33f1e7b2b57e
haveacupofjava19 <saketdeshmukh95@gmail.com>
2019-07-14 10:28:52 +0530

    A new Service created to handle generic validation error thus cleaning the Controller

--- PersonalProjectManagerTool/src/main/java/com/saket/ppmtool/web/ProjectController.java
+++ PersonalProjectManagerTool/src/main/java/com/saket/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.saket.ppmtool.web;
 
 import com.saket.ppmtool.domain.Project;
+import com.saket.ppmtool.services.MapValidationErrorService;
 import com.saket.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,19 +25,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
 
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        if(errorMap != null)
+            return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
