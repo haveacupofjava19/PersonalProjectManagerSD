0e7e10278b3cf1f5a9120e109d9d4c7d2c224179
haveacupofjava19 <saketdeshmukh95@gmail.com>
2019-07-27 01:24:52 +0530

    Project Task deleted

--- PersonalProjectManagerTool/src/main/java/com/saket/ppmtool/services/ProjectTaskService.java
+++ PersonalProjectManagerTool/src/main/java/com/saket/ppmtool/services/ProjectTaskService.java
@@ -105,11 +105,6 @@ public class ProjectTaskService {
     public void deleteByProjectSequence(String backlog_id, String pt_id){
         ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
 
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> projectTasksList = backlog.getProjectTasks();
-        projectTasksList.remove(projectTask);
-        backlogRepository.save(backlog);
-
         projectTaskRepository.delete(projectTask);
     }
 }
